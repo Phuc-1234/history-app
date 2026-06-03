3b06fda0cefc3bc90a52500f0e82f0ad7b01484f
TheHa4106 <hant04012006@gmail.com>
2026-06-03 21:50:06 +0700

    feat: forgot password screens (#43)

---
+++ apps/react-native-client/src/app/(1_auth)/1_4_otp_forgot.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { ForgotPasswordOtpScreen } from "@/features/forgot_password";
+
+export default function ForgotPasswordOtpRoute() {
+  return <ForgotPasswordOtpScreen />;
+}
