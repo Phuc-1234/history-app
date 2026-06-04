d422746d048e4fcd00c508ed7c52dd1b663182a4
Phuc-1234 <157017261+Phuc-1234@users.noreply.github.com>
2026-06-04 19:48:04 +0700

    fix pt8

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -1,6 +1,6 @@
 // packages/shared/src/index.ts
 
-import { PrismaClient } from "./generated/client";
+import { PrismaClient } from "@prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 import pg from "pg";
 import "dotenv/config";
