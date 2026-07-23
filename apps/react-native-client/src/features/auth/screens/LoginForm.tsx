fb5a2b2fd7a655885599d6ee303c048a5c8bb0e7
Huu Hoa <hoaro57@gmail.com>
2026-07-23 07:29:42 +0700

    Fix/login form (#164)
    * fix @gmail khi đăng nhập
    
    * update

--- apps/react-native-client/src/features/auth/screens/LoginForm.tsx
+++ apps/react-native-client/src/features/auth/screens/LoginForm.tsx
@@ -93,10 +93,10 @@ export default function LoginForm() {
                                 keyboardType="email-address"
                                 autoCapitalize="none"
                                 editable={!isLoading}
-                                // Thêm paddingRight vừa đủ để nội dung không đè lên @gmail.com
-                                style={[styles.customInput, { paddingRight: 95 }]}
+                                // Thêm paddingRight vừa đủ
+                                style={[styles.customInput, { paddingRight: (email.length > 0 && email.length < 15 && !email.includes("@")) ? 95 : 24 }]}
                             />
-                            {email.length > 0 && !email.includes("@") && (
+                            {email.length > 0 && email.length < 14 && !email.includes("@") && (
                                 <View style={styles.ghostTextWrapper} pointerEvents="none">
                                     <Text style={styles.ghostEmailText}>@gmail.com</Text>
                                 </View>
